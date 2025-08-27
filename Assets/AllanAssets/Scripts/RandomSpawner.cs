083f62e0e6a8b42725ca105be343e8525ad7629c
allangmrs <allan81.guima@gmail.com>
2025-08-27 10:09:11 -0300

    Difculdade nos spawners

--- Assets/AllanAssets/Scripts/RandomSpawner.cs
+++ Assets/AllanAssets/Scripts/RandomSpawner.cs
@@ -17,18 +17,37 @@ public class RandomSpawner : MonoBehaviour
 
     [SerializeField] private PosicaoDoSpawner posSpawner;
 
+    [SerializeField] private float intervaloInicial = 3.0f;
+    [SerializeField] private float intervaloMinimo = 0.5f;
+    [SerializeField] private float fatorDeDificuldade = 0.98f;
+    private float cronometroParaSpawn;
 
     void Awake()
     {
         areaDeSpawn = GetComponent<BoxCollider2D>();
     }
 
-    
+    private void Start()
+    {
+        cronometroParaSpawn = intervaloInicial;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        cronometroParaSpawn -= Time.deltaTime;
+
+        if (cronometroParaSpawn <= Mathf.Epsilon)
         {
             Spawn();
+
+            float tempoDecorrido = Time.timeSinceLevelLoad;
+
+            // 2. Calcula o intervalo atual usando a fórmula de decaimento exponencial.
+            float intervaloVariavel = (intervaloInicial - intervaloMinimo) * Mathf.Pow(fatorDeDificuldade, tempoDecorrido);
+            float intervaloAtual = intervaloMinimo + intervaloVariavel;
+
+            // 3. Reinicia o cronômetro com o novo valor calculado.
+            cronometroParaSpawn = intervaloAtual;
         }
     }
 
@@ -38,30 +57,33 @@ public class RandomSpawner : MonoBehaviour
 
         direcaoMovimento = Vector2.zero;
 
-        switch (posSpawner)
+        if(posSpawner == PosicaoDoSpawner.Cima || posSpawner == PosicaoDoSpawner.Baixo)
         {
+            posX = Random.Range(bounds.min.x, bounds.max.x);
+            posicaoSpawn = new Vector2(posX, transform.position.y);
+        }
+        else
+        {
+            posY = Random.Range(bounds.min.y, bounds.max.y);
+            posicaoSpawn = new Vector2(transform.position.x, posY);
+        }
+
+         switch (posSpawner)
+            {
             case PosicaoDoSpawner.Cima:
-                posX = Random.Range(bounds.min.x, bounds.max.x);
-                posicaoSpawn = new Vector2(posX, transform.position.y);
                 direcaoMovimento = Vector2.down;
                 break;
             case PosicaoDoSpawner.Baixo:
-                posX = Random.Range(bounds.min.x, bounds.max.x);
-                posicaoSpawn = new Vector2(posX, transform.position.y);
                 direcaoMovimento = Vector2.up;
                 break;
             case PosicaoDoSpawner.Direita:
-                posY = Random.Range(bounds.min.y, bounds.max.y);
-                posicaoSpawn = new Vector2(transform.position.x, posY);
                 direcaoMovimento = Vector2.left;
                 break;
             case PosicaoDoSpawner.Esquerda:
-                posY = Random.Range(bounds.min.y, bounds.max.y);
-                posicaoSpawn = new Vector2(transform.position.x, posY);
                 direcaoMovimento = Vector2.right;
                 break;
 
-        }
+            }
 
         GameObject novoInimigo = Instantiate(prefab, posicaoSpawn, Quaternion.identity);
 
